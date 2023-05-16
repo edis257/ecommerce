13237218d6542828ea398d3f672a481e91f46225
hi <50556680+edis257@users.noreply.github.com>
2023-05-16 14:35:07 +0300

    first day

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/", views.getRoutes, name="routes"),
+    path("", views.getRoutes, name="routes"),
+    path("products/", views.getProducts, name="products"),
+    path("products/<str:pk>/", views.getProduct, name="product"),
 ]
+
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from .products import products
+
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         '/api/products/',
@@ -17,4 +21,18 @@ def getRoutes(request):
         '/api/products/delete/<id>/',
         '/api/products/<update>/<id>/',
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    return Response(product)
